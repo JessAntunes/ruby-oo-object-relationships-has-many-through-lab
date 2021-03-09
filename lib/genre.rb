1ce8398d1c7941ce7eb4282ee0b2678bf7ad2afb
Jessica Antunes <jessantunes@protonmail.com>
2021-03-09 01:35:29 -0500

    commit tree 5e988563c715133058af0c93923497a21a5ef57a

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,27 @@
+require 'pry'
 
+class Genre
+    attr_accessor :name, :artist
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+    
+    def songs
+        Song.all.select { |song| song.genre = self }
+    end
+
+    def self.all
+        @@all
+    end
+
+    def artists
+        songs.map do |song|
+            song.artist
+        end
+    end
+
+end
